640b166d18f5b24475a55117ca27d7e2c447759d
Thiago Faria Moreira <106693084+thiagofm2@users.noreply.github.com>
2024-08-15 21:06:50 -0300

    login function working

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,30 @@
 'use client'
-
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from "react";
 import logo from '../../../public/logo.png';
 import Input from '../components/input';
+import { Button } from '@/components/ui/button';
+
+
 export default function Login() {
+
+    const [loginInfos, setloginInfos] = useState({ email: '', password: ''});
+    const router = useRouter()
+    
+    const handleLogin = async() => {
+        await axios.post(`https://reqres.in/api/login`, {
+            email: loginInfos.email,
+            password: loginInfos.password
+        })
+        .then((response) => {
+            router.push("/usuarios")
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+    };
+
     return(
         <>
             <div className="flex flex-grow h-full">
@@ -24,11 +46,23 @@ export default function Login() {
                         <h1 className='my-5'>
                             Login
                         </h1>
-                        <Input placeholder={'E-mail'} idInput={'email'} onChange={() => {}}  isPassword={false}/>
-                        <Input placeholder={'Senha'} idInput={'senha'}  onChange={() => {}}  isPassword={true}/>
-                        <button className='login-button'>
+                        <Input 
+                        placeholder={'E-mail'} 
+                        idInput={'email'} 
+                        value={loginInfos.email}
+                        onChange={(e: any) => setloginInfos({ ...loginInfos, email: e.target.value })}
+                        isPassword={false}/>
+                        <Input 
+                        placeholder={'Senha'} 
+                        idInput={'senha'}  
+                        value={loginInfos.password}
+                        onChange={(e: any) => setloginInfos({ ...loginInfos, password: e.target.value })}
+                        isPassword={true}/>
+                        <Button 
+                        onClick={() => handleLogin()}
+                        className='login-button'>
                             Logar
-                        </button>
+                        </Button>
                     </div>
                 </div>
             </div>
